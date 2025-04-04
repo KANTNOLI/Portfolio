6bdc6cc485b7801f25c6b1051e635cddf1533b56
KANTNOLI <zahargysev02@gmail.com>
2025-04-05 02:56:15 +0300

    v0.6.1

--- src/app/window/window.component.ts
+++ src/app/window/window.component.ts
@@ -8,11 +8,25 @@ import { RenderService } from '../service/render.service';
   styleUrl: './window.component.css',
 })
 export class WindowComponent {
-  private trackFlag: boolean = false;
+  // Тип треккер
+  private trackType: number = 0;
+  private funcLink!: (event: MouseEvent) => any;
+  // 0 off
+  // 1 drag
+  // 2 resizeT
+  // 3 resizeB
+  // 4 resizeR
+  // 5 resizeL
 
+  // Положение мышки при клике
   private AnalX: number = 0;
   private AnalY: number = 0;
 
+  // Размеры screen HTML главного обьекта, передаем статикой ибо мне похуй я заебался блять с этими 3д хуями
+  private width: number = 1600;
+  private height: number = 1000;
+
+  // HTML компоненты
   private screen: HTMLElement | null = null;
   private desktop: HTMLElement | null = null;
   private windowComp: HTMLElement | null = null;
@@ -25,10 +39,6 @@ export class WindowComponent {
   constructor(private renderService: RenderService) {}
 
   ngAfterContentInit() {
-    // Размеры screen HTML главного обьекта, передаем статикой ибо мне похуй я заебался блять с этими 3д хуями
-    const width = 1600;
-    const height = 1000;
-
     // Получение HTML компонентов для работы winHeader
     this.screen = document.querySelector('.screen'); // Главный экран весь
     this.desktop = document.querySelector('.desktop'); // Рабочий стол конкретно
@@ -39,40 +49,73 @@ export class WindowComponent {
     this.siteL = document.querySelector('.sideL'); // сторона Окна
     this.siteR = document.querySelector('.sideR'); // сторона Окна
 
-    // Получение ширину окна относительно сцены без погрешностей (Т.к. 1600 -> rand)
-    let desktopSizes = this.desktop?.getBoundingClientRect();
-
-    this.siteT?.addEventListener('mousedown', (event: MouseEvent) => {
-      this.AnalY = event.clientY;
-      this.AnalX = event.clientX;
-
-      console.log(event.clientX, event.clientY);
-
-      this.trackFlag = true;
-    });
+    this.dragWindowToggleActive();
 
     console.log(this.siteT);
+  }
 
-    if (this.trackFlag && this.desktop) {
-      this.desktop.addEventListener('mousemove', (event: MouseEvent) => {
-        if (this.windowComp && desktopSizes) {
-          let WRelativityOpposite =
-            (this.AnalX - desktopSizes.left) / desktopSizes.width;
-          let HRelativityOpposite =
-            (this.AnalY - desktopSizes.top) / desktopSizes.height;
+  dragWindowMove() {
+    let desktopSizes = this.desktop?.getBoundingClientRect();
+    let windowSizes = this.windowComp?.getBoundingClientRect();
+    console.log(windowSizes);
 
-          let HRelativity =
-            (event.clientY - desktopSizes.top) / desktopSizes.height;
-          let WRelativity =
-            (event.clientX - desktopSizes.left) / desktopSizes.width;
+    this.funcLink = (event: MouseEvent) => {
+      if (
+        this.trackType === 1 &&
+        this.windowComp &&
+        desktopSizes &&
+        windowSizes
+      ) {
+        let WRelativityOpposite =
+          ((this.AnalX - desktopSizes.left) / desktopSizes.width) * this.width;
+        let HRelativityOpposite =
+          ((this.AnalY - desktopSizes.top) / desktopSizes.height) * this.height;
 
-          this.windowComp.style.top = `${
-            HRelativity * height - HRelativityOpposite * height
-          }px`;
-          this.windowComp.style.left = `${
-            WRelativity * width - WRelativityOpposite * width
-          }px`;
-        }
+        let HRelativity =
+          ((event.clientY - desktopSizes.top) / desktopSizes.height) *
+          this.height;
+        let WRelativity =
+          ((event.clientX - desktopSizes.left) / desktopSizes.width) *
+          this.width;
+
+        let HWinRelativity =
+          ((windowSizes.top - desktopSizes.top) / windowSizes.height) *
+          this.height;
+        let WWinRelativity =
+          ((windowSizes.left - desktopSizes.left) / windowSizes.width) *
+          this.width;
+
+        this.windowComp.style.left = `${
+          WRelativity - WRelativityOpposite + WWinRelativity * 0.5
+        }px`;
+        this.windowComp.style.top = `${
+          HRelativity - HRelativityOpposite + HWinRelativity * 0.5
+        }px`;
+      }
+    };
+
+    if (this.funcLink) {
+      this.desktop?.addEventListener('mousemove', this.funcLink);
+    }
+  }
+
+  dragWindowToggleActive() {
+    if (this.winHeader) {
+      this.winHeader.addEventListener('mousedown', (event: MouseEvent) => {
+        this.AnalY = event.clientY;
+        this.AnalX = event.clientX;
+
+        this.trackType = 1;
+        this.dragWindowMove();
+        console.log('mousedown');
+      });
+
+      this.screen?.addEventListener('mouseup', (event: MouseEvent) => {
+        this.AnalY = event.clientY;
+        this.AnalX = event.clientX;
+        this.trackType = 0;
+        console.log('mouseup');
+        this.desktop?.removeEventListener('mousemove', this.funcLink);
       });
     }
   }
